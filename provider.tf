67adc262b0a28ca35d8302e51c7593da84a5cda1
root <root@DESKTOP-0JOSQMP.localdomain>
2023-11-20 15:05:05 +0530

    made some changes to the file

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
     region = var.ec2_region
-    AWS_ACCESS_KEY_ID = 'AKIA2HA3DDS7OJS25CPD'
-    AWS_SECRET_ACCESS_KEY = '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'
+    AWS_ACCESS_KEY_ID = "AKIA2HA3DDS7OJS25CPD"
+    AWS_SECRET_ACCESS_KEY = "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"
 }
